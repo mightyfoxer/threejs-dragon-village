01f9b9d13f49bd651b293864e130e0f8345a7642
mightyfoxer <azizamirul086@gmail.com>
2024-04-10 18:31:01 +1000

    feat: fixed App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { AboutPage, ContactPage, HomePage, ProjectsPage } from "@/pages";
+import { HomePage } from "@/pages";
 import { Navbar } from "@/components/_shared";
 
 export const App = () => {
